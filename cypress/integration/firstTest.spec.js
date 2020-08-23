42f6bec8a28ad9f3a2d1aa97aaa92f95e874ae8d
David Acero <acero.david.17@gmail.com>
2020-08-22 23:39:13 -0500

    New Test Cases

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+
 describe("First Test Suite", () => {
 
     it("Locators Test", () => {
@@ -111,7 +112,7 @@ describe("First Test Suite", () => {
         
     })
 
-    it.only("Invoke Command #2", () => {
+    it("Invoke Command #2", () => {
         cy.visit('/')
         cy.contains('Forms').click()         
         cy.contains('Datepicker').click()
@@ -128,7 +129,179 @@ describe("First Test Suite", () => {
                     .invoke('prop', 'value')
                     .should('contain', 'Aug 25, 2020')
             })
+    })
 
+    it("RadioButtons", () => {
+        cy.visit('/')
+        cy.contains('Forms').click()         
+        cy.contains('Form Layout').click()
+
+        cy.contains('nb-card', "Using the Grid")
+            .find('[type="radio"]')
+            .then(radioButtons => {
+                cy.wrap(radioButtons)
+                    .first() // eq(0)
+                    .check({force: true})
+                    .should('be.checked')
+                
+                cy.wrap(radioButtons)
+                    .eq(1) // index 1
+                    .check({force: true})
+                
+                cy.wrap(radioButtons)
+                    .first()
+                    .should('not.be.checked')
+
+                cy.wrap(radioButtons)
+                    .eq(2)
+                    .should('be.disabled')
+            })
+    })
+
+    it("Checkboxes", () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()         
+        cy.contains('Toastr').click()
+
+        cy.get("[type='checkbox']")
+            .check({force: true})
+            .uncheck({force: true})
         
+        cy.get("[type='checkbox']")
+            .eq(0)
+            .click({force: true})
+    })
+
+    it("Dropdown", () => {
+        cy.visit('/')
+        // Selecting 1 item
+        // cy.get('nav nb-select')
+        //     .click()
+        // cy.get('.options-list')
+        //     .contains('Dark')
+        //     .click()
+
+        // cy.get('nav nb-select')
+        //     .should('contain', 'Dark')
+
+        // cy.get('nb-layout-header nav')
+        //     .should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        // Looping per each
+        cy.get('nav nb-select')
+            .then(dropdown => {
+                cy.wrap(dropdown).click()
+                cy.get('.options-list nb-option')
+                    .each( (listItem, index) => {
+                        const itemText = listItem.text().trim()
+                        const colors = {
+                            "Light" : "rgb(255, 255, 255)",
+                            "Dark" : "rgb(34, 43, 69)",
+                            "Cosmic": "rgb(50, 50, 89)",
+                            "Corporate" : "rgb(255, 255, 255)"
+                        }
+
+                        cy.wrap(listItem).click()
+                        cy.wrap(dropdown)
+                            .should('contain', itemText)
+                        cy.get('nb-layout-header nav')
+                            .should('have.css', 'background-color', colors[itemText])
+                        
+                        if( index < 3){
+                            cy.wrap(dropdown).click()                 
+                        }
+                    })
+            })
+        
+        // Also you can use cypress Select
+    })
+
+    it.only("Table #1", () => {
+
+        cy.visit('/')
+        cy.contains('Tables & Data').click()         
+        cy.contains('Smart Table').click()
+
+        // Edit Value and Verify it
+        cy.get('tbody')
+            .contains('tr', 'Larry')
+            .then(tRow => {
+                cy.wrap(tRow)
+                    .find('.nb-edit')
+                    .click()
+
+                cy.wrap(tRow)
+                    .find('[placeholder="Age"]')
+                    .clear()
+                    .type('25')
+                
+                cy.wrap(tRow)
+                    .find('.nb-checkmark')
+                    .click()
+
+                cy.wrap(tRow)
+                    .find('td')
+                    .eq(6)
+                    .should('contain', '25')
+            })
+
+        // Add user
+        cy.get('thead')
+            .find('.nb-plus')
+            .click()
+        
+        cy.get('thead')
+            .find('tr')
+            .eq(2)
+            .then( tRow => {
+                cy.wrap(tRow)
+                    .find('[placeholder="First Name"]')
+                    .type('David')
+
+                cy.wrap(tRow)
+                    .find('[placeholder="Last Name"]')
+                    .type('Acero')
+       
+                cy.wrap(tRow)
+                    .find('.nb-checkmark')
+                    .click()
+            })
+        
+        cy.get('tbody tr')
+            .first()
+            .find('td')
+            .then(tColumn => {
+                cy.wrap(tColumn)
+                    .eq(2)
+                    .should('contain', 'David')
+
+                cy.wrap(tColumn)
+                    .eq(3)
+                    .should('contain', 'Acero')
+                    
+            })
+
+        // Verify Search Tab
+        const age = [20, 30, 40, 200]
+
+        cy.wrap(age).each(age => {
+            cy.get('thead [placeholder="Age"]')
+                .clear()
+                .type(age)
+                .wait(500)
+        
+            cy.get('tbody tr')
+                .each(tRow => {
+                    if(age == 200){
+                        cy.wrap(tRow)
+                            .should('contain', 'No data found')
+                    } else{
+                        cy.wrap(tRow)
+                            .find('td')
+                            .eq(6)
+                            .should('contain', age)
+                    }
+            })
+        })
     })
 })
